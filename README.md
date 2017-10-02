892acc97fac1f9179d85c4572db63a47942cc277
Julian Xhokaxhiu <xhokaxhiujulian@gmail.com>
2017-10-02 17:41:52 +0200

    0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # dynsdjs-plugin-docker
 Docker plugin for dynsdjs
+
+## Installation
+
+Just install the package via npm by doing
+
+```shell
+$ npm install -g dynsdjs-plugin-docker
+```
+
+## Options
+
+You can configure this plugin through Environment variables
+
+- `DOCKER_HOST` will tell to the plugin the path to your Docker Host in order to connect to ( see the Docker Daemon [official documentation](https://docs.docker.com/engine/reference/commandline/dockerd/#examples) for more )
+
+## Usage
+
+By default this plugin requires no further configuration.
+
+Just run `dynsdjs` and enjoy.
+
+## Workflow
+
+When the plugin starts, it will connect to your Docker daemon and it will listen for any event that has to do with Container Start or Stop.
+
+As soon as the container is started or stopped, it will use the ID to fetch the full container configuration payload. Once the payload is received, a basic set of informations are collected, such as container Name, Hostname, IP, Environment variables or Labels. Both IPv4 and IPv6 records are supported.
+
+Once this payload is built, the domain will be fetched ( see the relative section down below ) and finally, depending on the status, it will do the following action:
+
+- If Started, the domain will be added with its relative `A` and `AAAA` records,
+- If Stopped, the domain will be simply removed
+
+These actions will be done for any Container, as long as `dynsd` will be up and running.
+
+### Domain fetch
+
+The domain fetch from your container will be done in this particular order:
+
+1. First it will try to see if the [jwilder/nginx-proxy](https://github.com/jwilder/nginx-proxy) `VIRTUAL_HOST` environment variable is there, and if yes, it will use that as domain. If not found,
+2. It will use the container name
+
+## Example
+
+Dynsd side:
+
+```shell
+$ dynsd
+[18:08:10] INFO: [CORE] Loading plugin 'dynsdjs-plugin-docker'...
+[18:08:11] INFO: [CORE] Dispatching 'init' event. Waiting for plugins to complete...
+[18:08:11] INFO: [DOCKER] Connected to Docker API...
+[18:08:11] INFO: [DNS] Listening on [::]:53/tcp
+[18:08:11] INFO: [DNS] Listening on 0.0.0.0:53/udp
+[18:08:11] INFO: [DNS] Listening on [::]:53/udp
+[18:08:19] INFO: [DOCKER] Added container with domain 'nginx.test'...
+[18:08:23] INFO: [DOCKER] Removed container with domain 'nginx.test'...
+```
+
+Docker side:
+
+```shell
+$ docker run -d --rm --name nginx.test nginx:alpine
+# ...
+$ docker stop nginx.test
+```
---
+++ src/index.js
@@ -0,0 +1,113 @@
+import Dockerode from 'dockerode'
+import DockerEvents from 'docker-events'
+
+// We will use this to store a reference to the dynsd chalk instance
+let chalk = null
+
+// We will use this to store a reference to the dynsd cache instance
+let entries = null
+
+// Inherit configurations through existing environment variables, especially DOCKER_HOST
+const docker = new Dockerode()
+
+// Our docker emitter handler to the API
+const dockerEmitter = new DockerEvents({
+  docker: docker
+})
+
+// Build the DNS entry from the container infos
+const buildInfo = ( status, name, data ) => {
+  const action = ( status === 'stop' ? 'Removed' : 'Added' ),
+        container = {
+          A: data ? data.NetworkSettings.Networks.bridge.IPAddress : null,
+          AAAA: data ? data.NetworkSettings.Networks.bridge.GlobalIPv6Address : null,
+          name: name,
+          envs: data ? data.Config.Env : null,
+          labels: data ? data.Config.Labels : null
+        }
+
+  let entry = {}
+
+  // Support jwilder nginx-proxy companion ENV vars
+  container.envs
+    .forEach(
+      env => {
+        if ( env.indexOf( 'VIRTUAL_HOST' ) !== -1 )
+          container.domain = env.split('=')[1]
+      }
+    )
+
+  // If we still have no domain set, we use the generated name as fallback
+  if ( !container.domain )
+    container.domain = container.name
+
+  // Add or remove the entry from the DNS
+  if ( status === 'stop' )
+    entries
+      .del( container.domain )
+  else {
+    // Build the entry
+    if ( container.A )
+      entry.A = {
+        name: container.domain,
+        address: container.A,
+        ttl: 600
+      }
+
+    if ( container.AAAA )
+      entry.AAAA = {
+        name: container.domain,
+        address: container.AAAA,
+        ttl: 600
+      }
+
+    // Add the entry
+    entries
+      .set(
+        container.domain,
+        entry
+      )
+  }
+
+  console.log( `[${chalk.blue('DOCKER')}] ${action} container with domain '${chalk.green(container.domain)}'...` )
+}
+
+// Fetch detailed container infos
+const fetchInfo = container => {
+  docker
+    .getContainer( container.id )
+    .inspect(
+      ( err, data ) => {
+        if ( err )
+          container.status = 'stop'
+
+        buildInfo( container.status, container.Actor.Attributes.name, data )
+      }
+    )
+}
+
+// Initialize the Docker API listener
+const start = ( resolve, reject, data ) => {
+  entries = data.entries
+
+  dockerEmitter
+    .on( 'connect', () => {
+      console.log( `[${chalk.blue('DOCKER')}] Connected to Docker API...` )
+      resolve()
+    })
+    .on( 'start', data => fetchInfo( data ) )
+    .on( 'stop', data => fetchInfo( data ) )
+    .on( 'error', reject )
+    .start()
+}
+
+export default class {
+  constructor( dns ) {
+    chalk = dns.chalk
+
+    dns
+      .on( 'init', ( resolve, reject, data ) => {
+        start( resolve, reject, data )
+      })
+  }
+}
